bb408126dd0572f8f63512e5324446f4e9d7a47a
Dhruvil Gajjar <dgajjar999@gmail.com>
2020-02-07 18:45:28 +0530

    add product data & reducer & action

---
+++ screens/shop/ProductsOverviewScreen.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, FlatList } from 'react-native';
+
+const ProductOverViewScreen = () => {
+	return <FlatList />;
+};
+
+const styles = StyleSheet.create({});
+
+export default ProductOverViewScreen;
