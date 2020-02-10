04b2d939d3dfe095634b2a46c7ca9d3371b83ad5
Dhruvil Gajjar <dgajjar999@gmail.com>
2020-02-10 11:38:24 +0530

    add content in product overview screen

--- screens/shop/ProductsOverviewScreen.jsx
+++ screens/shop/ProductsOverviewScreen.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, FlatList, Text } from 'react-native';
 import { useSelector } from 'react-redux';
+import ProductItem from '../../components/shop/ProductItem';
 
 const ProductOverViewScreen = () => {
 	const products = useSelector(state => state.products.availableProducts);
@@ -8,7 +9,15 @@ const ProductOverViewScreen = () => {
 	return (
 		<FlatList
 			data={products}
-			renderItem={itemData => <Text>{itemData.item.title}</Text>}
+			renderItem={itemData => (
+				<ProductItem
+					image={itemData.item.imageUrl}
+					title={itemData.item.title}
+					price={itemData.item.price}
+					onViewDetail={() => {}}
+					onAddToCart={() => {}}
+				/>
+			)}
 		/>
 	);
 };
