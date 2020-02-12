374fa0862d2a69922522769bb8c125a0d989b517
Dhruvil Gajjar <dgajjar999@gmail.com>
2020-02-12 14:13:26 +0530

    add order item component

---
+++ components/shop/OrderItem.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+const OrderItem = () => {
+  return (
+    <View>
+      <View>
+        <Text></Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default OrderItem;
