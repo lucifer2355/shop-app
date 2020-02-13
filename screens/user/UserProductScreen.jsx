8f65ca94de19b5a889a704cfa7184a0d1b2e110c
Dhruvil Gajjar <dgajjar999@gmail.com>
2020-02-13 15:46:36 +0530

    chnage product item comoponent

--- screens/user/UserProductScreen.jsx
+++ screens/user/UserProductScreen.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { FlatList, Platform } from "react-native";
+import { FlatList, Platform, Button } from "react-native";
 import { useSelector } from "react-redux";
 import ProductItem from "../../components/shop/ProductItem";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import HeaderButton from "../../components/UI/HeaderButton";
+import Colors from "../../constants/Colors";
 
 const UserProductScreen = () => {
   const userProducts = useSelector(state => state.products.userProducts);
@@ -16,9 +17,11 @@ const UserProductScreen = () => {
           image={itemData.item.imageUrl}
           title={itemData.item.title}
           price={itemData.item.price}
-          onViewDetail={() => {}}
-          onAddToCart={() => {}}
-        />
+          onSelect={() => {}}
+        >
+          <Button color={Colors.primary} title='Edit' onPress={() => {}} />
+          <Button color={Colors.primary} title='Delete' onPress={() => {}} />
+        </ProductItem>
       )}
     />
   );
