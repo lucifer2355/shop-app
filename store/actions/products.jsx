187ed04efe19aa8d1bc68449c885c7838d2d253d
Dhruvil Gajjar <dgajjar999@gmail.com>
2020-02-18 18:44:26 +0530

    add update & delete action from the firebase

--- store/actions/products.jsx
+++ store/actions/products.jsx
@@ -39,7 +39,16 @@ export const fetchProducts = () => {
 };
 
 export const deleteProduct = productId => {
-  return { type: DELETE_PRODUCT, pid: productId };
+  return async dispatch => {
+    await fetch(
+      `https://shop-app-29bba.firebaseio.com/products/${productId}.json`,
+      {
+        method: "DELETE"
+      }
+    );
+
+    dispatch({ type: DELETE_PRODUCT, pid: productId });
+  };
 };
 
 export const createProduct = (title, description, imageUrl, price) => {
@@ -76,13 +85,27 @@ export const createProduct = (title, description, imageUrl, price) => {
 };
 
 export const updateProduct = (id, title, description, imageUrl) => {
-  return {
-    type: UPDATE_PRODUCT,
-    pid: id,
-    productData: {
-      title,
-      description,
-      imageUrl
-    }
+  return async dispatch => {
+    await fetch(`https://shop-app-29bba.firebaseio.com/products/${id}.json`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        title,
+        description,
+        imageUrl
+      })
+    });
+
+    dispatch({
+      type: UPDATE_PRODUCT,
+      pid: id,
+      productData: {
+        title,
+        description,
+        imageUrl
+      }
+    });
   };
 };
