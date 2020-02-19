40f091f2d892a60b3f78f17a5a80b0c890d8eccb
Dhruvil Gajjar <dgajjar999@gmail.com>
2020-02-19 11:59:28 +0530

    fetch order from the firebase & add loading screen animation in order screen

--- store/actions/orders.jsx
+++ store/actions/orders.jsx
@@ -1,4 +1,38 @@
 export const ADD_ORDER = "ADD_ORDER";
+import Order from "../../models/order";
+export const SET_ORDERS = "SET_ORDERS";
+
+export const fetchOrders = () => {
+  return async dispatch => {
+    try {
+      const response = await fetch(
+        "https://shop-app-29bba.firebaseio.com/orders/u1.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const resData = await response.json();
+      const loadedOrders = [];
+
+      for (const key in resData) {
+        loadedOrders.push(
+          new Order(
+            key,
+            resData[key].cartItems,
+            resData[key].totalAmount,
+            new Date(resData[key].date)
+          )
+        );
+      }
+
+      dispatch({ type: SET_ORDERS, orders: loadedOrders });
+    } catch (err) {
+      throw err;
+    }
+  };
+};
 
 export const addOrder = (cartItems, totalAmount) => {
   return async dispatch => {
